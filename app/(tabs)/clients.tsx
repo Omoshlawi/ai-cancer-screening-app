8bc3e9e03bb3f3d425e8772a27b68587e0c7fe4a
Laurent Ouma <lawiomosh3@gmail.com>
2025-11-18 23:15:58 +0300

    Add dayjs dependency for date handling; enhance Clients screen to display client information with age and next follow-up date using FlatList and improved UI components.

--- app/(tabs)/clients.tsx
+++ app/(tabs)/clients.tsx
@@ -1,13 +1,64 @@
 import { ClientFilter } from "@/components/client";
 import { CHPLandingScreenLayout } from "@/components/layout";
 import { Box } from "@/components/ui/box";
-import { Button, ButtonText } from "@/components/ui/button";
+import { Button, ButtonIcon, ButtonText } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { HStack } from "@/components/ui/hstack";
 import { Icon } from "@/components/ui/icon";
+import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
+import dayjs from "dayjs";
 import { router } from "expo-router";
-import { UserPlus } from "lucide-react-native";
+import {
+  ArrowRight,
+  Calendar,
+  Dot,
+  Phone,
+  UserPlus,
+} from "lucide-react-native";
 import React, { useState } from "react";
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const clients = [
+  {
+    id: 1,
+    name: "Mary Wanjiku",
+    level: "medium",
+    dob: "1990-01-01",
+    phone: "0712345678",
+    nationalId: "1234567890",
+    nextFollowUp: "2025-12-01",
+  },
+  {
+    id: 2,
+    name: "Sarah Achieng'",
+    level: "medium",
+    dob: "1990-01-01",
+    phone: "0712345678",
+    nationalId: "1234567890",
+    nextFollowUp: "2025-12-01",
+  },
+  {
+    id: 3,
+    name: "Grace Nyakobi",
+    level: "medium",
+    dob: "1990-01-01",
+    phone: "0712345678",
+    nationalId: "1234567890",
+    nextFollowUp: "2025-12-01",
+  },
+  {
+    id: 4,
+    name: "Alice Muthoni",
+    level: "medium",
+    dob: "1990-01-01",
+    phone: "0712345678",
+    nationalId: "1234567890",
+    nextFollowUp: "2025-12-01",
+  },
+];
 
 const ClientsScreen = () => {
   const [search, setSearch] = useState("");
@@ -16,7 +67,7 @@ const ClientsScreen = () => {
     <SafeAreaView className="flex-1 bg-background-0">
       <CHPLandingScreenLayout>
         <Box className="flex-1 p-4">
-          <VStack space="md">
+          <VStack space="md" className="flex-1">
             <Button
               action="default"
               className="border border-dashed border-teal-500 bg-background-0"
@@ -33,6 +84,75 @@ const ClientsScreen = () => {
               onSearchChange={setSearch}
               onLevelChange={setLevel}
             />
+            <Box className="flex-1 ">
+              <FlatList
+                data={clients}
+                keyExtractor={(item) => item.id.toString()}
+                ItemSeparatorComponent={() => <Box className="h-2" />}
+                renderItem={({ item }) => {
+                  const age = dayjs().diff(dayjs(item.dob), "years");
+                  return (
+                    <Card size="md" variant="elevated">
+                      <VStack space="md">
+                        <HStack className="justify-between items-center">
+                          <Heading size="sm">{item.name}</Heading>
+                          <Text
+                            size="xs"
+                            className="text-typography-500 bg-teal-100 px-2 py-1 rounded-full"
+                          >
+                            {item.level} Risk
+                          </Text>
+                        </HStack>
+                        <HStack className="items-center" space="lg">
+                          <Text size="sm" className="text-typography-500">
+                            Age: {age}
+                          </Text>
+                          <Icon
+                            as={Dot}
+                            size="sm"
+                            className="text-typography-500"
+                          />
+                          <Text size="sm" className="text-typography-500">
+                            Id: {item.nationalId}
+                          </Text>
+                        </HStack>
+                        <HStack className="items-center" space="lg">
+                          <Icon
+                            as={Phone}
+                            size="xs"
+                            className="text-typography-500"
+                          />
+                          <Text size="sm" className="text-typography-500">
+                            {item.phone}
+                          </Text>
+                        </HStack>
+                        <HStack className="justify-between">
+                          <HStack className="items-center" space="lg">
+                            <Icon
+                              as={Calendar}
+                              size="xs"
+                              className="text-typography-500"
+                            />
+                            <Text size="sm" className="text-typography-500">
+                              Next follow-up:{" "}
+                              {dayjs(item.nextFollowUp).format("DD/MM/YYYY")}
+                            </Text>
+                          </HStack>
+                          <Button
+                            action="positive"
+                            size="sm"
+                            className="bg-teal-500"
+                          >
+                            <ButtonText>View</ButtonText>
+                            <ButtonIcon as={ArrowRight} size="sm" />
+                          </Button>
+                        </HStack>
+                      </VStack>
+                    </Card>
+                  );
+                }}
+              />
+            </Box>
           </VStack>
         </Box>
       </CHPLandingScreenLayout>
